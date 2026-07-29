3429c423f8961008e3e81ba7dba880b4f66ec2ee
Usman-17 <usmandev580@gmail.com>
2026-07-29 10:16:20 +0500

    feat: Update Hero component to include mobile hero image

--- frontend/src/pages/HomePage/sections/Hero.jsx
+++ frontend/src/pages/HomePage/sections/Hero.jsx
@@ -1,10 +1,13 @@
 import TextMarquee from "./TextMarquee";
 import hero from "../../../assets/hero.webp";
+import heroMobile from "../../../assets/hero-m.webp";
+
 const Hero = () => {
   return (
     <div className="-mx-3">
-      <div className="w-full h-170 sm:h-75 lg:h-160 overflow-hidde">
-        <img src={hero} alt="Banner" className="w-full h-full object-cover" />
+      <div className="w-full h-170 sm:h-75 lg:h-160 overflow-hidden">
+        <img src={heroMobile} alt="Banner" className="w-full h-full object-cover sm:hidden" />
+        <img src={hero} alt="Banner" className="hidden sm:block w-full h-full object-cover" />
       </div>
 
       {/* Marquee */}
